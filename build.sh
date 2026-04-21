a372ef1bf6ee3b34318556d6e1a280a196e901d2
sandeepkumar9760 <sandeeproofingsarvice@gmail.com>
2026-04-21 10:50:13 +0530

    Update admin username to 9647118535 in build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 
 # Create a default admin user if it doesn't exist (Free alternative to Shell)
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='7001701818').exists() or User.objects.create_superuser('7001701818', 'admin@example.com', 'admin123')"
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='9647118535').exists() or User.objects.create_superuser('9647118535', 'admin@example.com', 'admin123')"
